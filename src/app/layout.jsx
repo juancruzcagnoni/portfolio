2261b0e66876d9c1cedaadadd7c9ad5eaec18b11
Juan Cruz Cagnoni <juancagnoni@gmail.com>
2024-10-23 19:06:48 -0300

    metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,13 +4,19 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Juan Cruz Cagnoni — Frontend Developer & UX/UI Designer",
-  description: "Portfolio",
+  title: "Juan Cruz Cagnoni — Developer & Designer",
+  description: "Portfolio of Juan Cruz Cagnoni, a passionate developer and designer creating innovative digital solutions. Explore my projects and skills in web development and UX/UI design.",
+  icons: {
+    icon: '/icon.png',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-primary">
+      <head>
+        <link rel="icon" href="/icon.png" />
+      </head>
       <body className={inter.className}>
         {children}
       </body>
