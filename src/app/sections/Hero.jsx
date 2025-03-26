c64b311e579d78c19e4a5c1f83ba103687c6fb70
unknown <juancagnoni@gmail.com>
2025-03-26 11:43:46 -0300

    appos

--- src/app/sections/Hero.jsx
+++ src/app/sections/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <section className="relative left-1/2 transform -translate-x-1/2 w-[90%] md:w-[70%] lg:w-[30%] flex justify-between mt-16 md:mt-24 mb-12">
       <div className="space-y-4 md:space-y-6">
         <p className="text-sm md:text-base text-primary dark:text-secondary">
-          I'm from San Antonio de Areco, Buenos Aires, Argentina. I studied Web Design and Programming at Escuela Da Vinci, a three-year technical degree.
+          I&apos;m from San Antonio de Areco, Buenos Aires, Argentina. I studied Web Design and Programming at Escuela Da Vinci, a three-year technical degree.
         </p>
 
         <p className="text-sm md:text-base text-primary dark:text-secondary">
