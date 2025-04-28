8d0519bd5e5308e66f7b88ac565047fe9d0d94dc
unknown <juancagnoni@gmail.com>
2025-04-28 17:37:47 -0300

    Analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { GeistSans } from 'geist/font/sans'
 import Footer from './components/Footer'
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 export const metadata = {
