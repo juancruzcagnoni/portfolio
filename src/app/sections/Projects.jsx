63de9faebfeef8f9efda4a4cf1fc8e2a5ec7ecd2
unknown <juancagnoni@gmail.com>
2025-06-25 17:31:27 -0300

    ajustes

--- src/app/sections/Projects.jsx
+++ src/app/sections/Projects.jsx
@@ -1,40 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-
-export default function Projects({ onLoad }) {
-  const [projects, setProjects] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const response = await fetch('/api/projects');
-        const data = await response.json();
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        setProjects(data);
-      } catch (error) {
-        console.error('Error details:', error);
-        setError(error.message);
-      } finally {
-        setLoading(false);
-        onLoad && onLoad();
-      }
-    };
-
-    fetchProjects();
-  }, [onLoad]);
-
-  if (loading || error) {
-    return null;
-  }
-
+export default function Projects() {
   return (
     <section className="relative left-1/2 transform -translate-x-1/2 w-[90%] md:w-[70%] lg:w-[40%] py-8 md:py-10">
       <h2 className="font-instrument-serif text-2xl md:text-3xl text-primary dark:text-secondary">
